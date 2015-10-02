c5239914dedcf258b6fb0716c44d8e212a4c29ed
ryanbelt <ryanbelt1993129@hotmail.com>
2015-10-02 16:04:21 -0400

    nothing change

--- public/js/models/movie.js
+++ public/js/models/movie.js
@@ -9,7 +9,7 @@ splat.MovieModel = Backbone.Model.extend({
 
 
       defaults: {
-        /*
+        
         title: '', //movie title
         released: null, //release year in Date
         director:'', //movie's director
@@ -23,20 +23,5 @@ splat.MovieModel = Backbone.Model.extend({
       	trailer : null,  // URL for trailer/movie-streaming
       	poster : "img/placeholder.png",  // movie-poster image URL
       	dated: new Date()  // date of movie posting
-        */
-        title (string: "")  // movie title
-      released (Date: null)  // release year
-      director (string: "")  // movie's director
-      starring (string array: [])  // array principal actors
-      rating (string: "")  // MPAA movie rating: G, PG, PG-13, R, NC-17, NR
-      duration (number: null)   // run-time in minutes
-      genre (string array: [])   // genre terms, e.g. action, comedy, etc
-      synopsis (string: "")  // brief outline of the movie
-      freshTotal (number: 0.0)   // cumulative total of review fresh (1.0) votes
-      freshVotes (number: 0.0)   // number of review ratings
-      trailer (string: null)  // URL for trailer/movie-streaming
-      poster (string: "img/placeholder.png")  // movie-poster image URL
-      dated (Date: new Date() )  // date of movie posting
-
       }
 });
